4f3ac4218ca3502f319141db4ee4eaaef411e913
Sigmarik <itkudryashov@gmail.com>
2025-10-07 17:17:47 +0300

    feat: copied local_shadertoy1 to local_shadertoy2

---
+++ tasks/local_shadertoy2/App.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <chrono>
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+#include <etna/Sampler.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+
+class App
+{
+public:
+  App();
+  ~App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+  void updateParams();
+
+private:
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  glm::uvec2 resolution;
+  bool useVsync;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  struct ShaderParams
+  {
+    uint32_t resolutionX, resolutionY;
+    float mouseX, mouseY;
+    float time;
+  };
+
+  etna::Sampler sampler;
+  etna::Image result;
+  etna::ComputePipeline pipeline;
+
+  ShaderParams params;
+
+  std::chrono::steady_clock::time_point startTime;
+};
