ec113ff678116600d01eb74686c88f4cd6ce86c2
Sigmarik <itkudryashov@gmail.com>
2025-11-25 01:12:03 +0300

    feat: basic tonemapping setup

---
+++ tasks/tonemapping/App.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "etna/BlockingTransferHelper.hpp"
+#include "etna/GraphicsPipeline.hpp"
+
+
+#include <chrono>
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/Image.hpp>
+#include <etna/Sampler.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+
+class App
+{
+public:
+  App();
+  ~App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+  void updateParams();
+
+  void importTextures();
+
+private:
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  glm::uvec2 resolution;
+  bool useVsync;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+  struct ShaderParams
+  {
+    uint32_t resolutionX, resolutionY;
+    float mouseX, mouseY;
+    float time;
+  };
+
+  struct TonemappingParams
+  {
+    uint32_t resolutionX, resolutionY;
+    float avg, var;
+  };
+
+  etna::Sampler sampler;
+
+  etna::Image skyTexture;
+  etna::Image ballTexture;
+  static constexpr glm::uvec2 BALL_TEXTURE_RESOLUTION{2048, 2048};
+
+  etna::Image hdrFrame;
+
+  etna::GraphicsPipeline intermediatePipeline;
+  etna::GraphicsPipeline mainPipeline;
+  etna::GraphicsPipeline toneMappingPipeline;
+
+  ShaderParams params;
+  TonemappingParams tonemappingParams;
+
+  std::chrono::steady_clock::time_point startTime;
+};
---
+++ tasks/tonemapping/shaders/tonemap.frag
@@ -0,0 +1,47 @@
+#version 450
+
+layout(binding = 0) uniform sampler2D iSource;
+
+layout(location = 0) out vec4 out_fragColor;
+
+layout(push_constant) uniform params
+{
+  uint resolutionX;
+  uint resolutionY;
+  float avgBrightness;
+  float varBrightness;
+} params_t;
+
+layout(location = 0) in VS_OUT
+{
+  vec2 wPos;
+} surf;
+
+ivec3 iResolution()
+{
+  return ivec3(params_t.resolutionX, params_t.resolutionY, 0);
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  vec2 uv = fragCoord / iResolution().xy;
+
+  vec4 source = texture(iSource, uv);
+
+  vec3 color = source.rgb;
+  float value = length(source.rgb);
+  float transformedBrightness = (value - params_t.avgBrightness) / params_t.varBrightness / 3.0 + params_t.avgBrightness;
+  fragColor = vec4(color * transformedBrightness, 1.0);
+}
+
+void main()
+{
+  vec2 pos = surf.wPos / 2.0 + vec2(0.5);
+//  pos.y = 1.0 - pos.y;
+  pos = pos * vec2(iResolution());
+
+  vec4 fragColor = vec4(0.0);
+  mainImage(fragColor, pos);
+
+  out_fragColor = fragColor;
+}
